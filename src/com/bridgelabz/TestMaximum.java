dedf4d90eb4c05038f203f0efcae0772b5ca44b7
drisyaathul <drisyak809@gmail.com>
2023-03-21 00:56:49 +0800

    Added: Extended the maximum method to take more than 3 parameters

--- src/com/bridgelabz/TestMaximum.java
+++ src/com/bridgelabz/TestMaximum.java
@@ -1,28 +1,39 @@
 package com.bridgelabz;
 
 public class TestMaximum <T extends Comparable <T>>{
-    T a,b,c;
+    T a,b,c,d,e,f;
 
-    public TestMaximum(T a, T b, T c){
+    public TestMaximum(T a, T b, T c, T d, T e, T f){
         this.a = a;
         this.b = b;
         this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
+
     }
-    static <T extends Comparable <T>> T maximumValue(T a, T b, T c){
+    static <T extends Comparable <T>> T maximumValue(T a, T b, T c, T d, T e, T f){
 
         T max = a;
         if (b.compareTo(max) > 0)
-        max = b;
+            max = b;
         if (c.compareTo(max) > 0)
-        max = c;
+            max = c;
+        if (d.compareTo(max) > 0)
+            max = d;
+        if (e.compareTo(max) > 0)
+            max = e;
+        if (f.compareTo(max) > 0)
+            max = f;
+
         return max;
     }
     public static void main(String[] args) {
         System.out.println("*** Welcome to Generics Problem ***");
 
-        System.out.println("The Maximum Value in Three Integers : "+maximumValue(20, 80, 60));
-        System.out.println("The Maximum Value in Three Floats : "+maximumValue(7.29f, 3.10f, 9.23f));
-        System.out.println("The Maximum Value in Three Strings : "+maximumValue("Apple", "Peach", "Banana"));
+        System.out.println("The Maximum Value in Integers : "+maximumValue(20, 80, 90, 50,40, 75));
+        System.out.println("The Maximum Value in Floats : "+maximumValue(7.29f, 3.10f, 9.23f, 9.9f, 2.8f, 4.7f));
+        System.out.println("The Maximum Value in Strings : "+maximumValue("Apple","Peach","Banana","Orange","WaterMelon","Grapes"));
 
     }
 }
